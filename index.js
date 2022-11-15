23211fb8fbcb75d10a691da153153dc18dbff3a3
Ash Jones <ashjones0515@gmail.com>
2022-11-15 15:28:50 -0800

    setup express routing for API

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const videosRoutes = require('./routes/videos')
+const PORT = 2000;
+
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.json())
+app.use((req,res,next)=>{
+	console.log("middleware")
+	next()
+})
 
 
+app.use('/videos',videosRoutes)
 
 
-
-app.listen(5050, function () {
-	console.log('server running at http://localhost:' + 5050);
+app.listen(PORT, function () {
+	console.log(`server running at http://localhost:${PORT}`);
 });
---
+++ routes/videos.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const {v4: uuid} = require('uuid');
+
+router.route('/')
+    .get((req, res) => {
+	const data = fs.readFileSync('./data/videos.json', 'utf-8');
+	const videoListData = JSON.parse(data).map((video) => {
+		return {
+			id: video.id,
+			title: video.title,
+			channel: video.channel,
+			image: video.image,
+		};
+	});
+	res.json(videoListData);
+})
+    .post((req,res)=>{
+        const data =fs.readFileSync('./data/videos.json', 'utf-8');
+        const videoListData = JSON.parse(data);
+        if(req.body.title && req.body.desc){
+            videoListData.push({
+                id: uuid(),
+                title: req.body.title,
+                channel: "Ash Jones",
+                image: req.body.image,
+                description: req.body.desc,
+                views: "0",
+                duration: "12:34",
+                video:"https://project-2-api.herokuapp.com/stream",
+                timestamp: Date.now(),
+                comments:[
+                    {
+                        id: uuid(),
+                        name: "Santa",
+                        comment: "You're gonna get coal in your stocking",
+                        likes: 0,
+                        timestamp: Date.now()
+                    }
+                ]
+            })
+            fs.writeFileSync('./data/videos.json', JSON.stringify(videoListData))
+            res.send("Video published to server")
+        } else {
+            res.send("You forgot to include json data in your request")
+        }
+    })
+
+router.route('/:id').get((req, res) => {
+	const data = fs.readFileSync('./data/videos.json', 'utf-8');
+	const videoData = JSON.parse(data);
+	res.json(
+		videoData.find((video) => {
+			return video.id === req.params.id;
+		})
+	);
+});
+
+module.exports = router;
